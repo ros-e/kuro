7255bb5acba9c5a6a12f5421f94945b85b6c5025
ros-e <176725295+ros-e@users.noreply.github.com>
2026-03-04 10:26:22 -0600

    feat: merge root.go into main.go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/ros-e/kuro/cmd"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "kuro",
+	Short: "Self-Hosted PaaS alternative to Vercel and Heroku",
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
 func main() {
-	cmd.Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
