948e0deaa27cdff471d021bb93fe2741778de618
ros-e <176725295+ros-e@users.noreply.github.com>
2026-03-04 18:40:32 -0600

    feat: config structs and types

---
+++ internal/config.go
@@ -0,0 +1,42 @@
+package internal
+
+var RootConfigPath = "/etc/kuro/config.yaml"
+
+type Provider string
+type Trigger string
+
+const (
+	ProviderGitHub Provider = "github"
+	ProviderGitLab Provider = "gitlab"
+	ProviderGitea  Provider = "gitea"
+)
+
+const (
+	TriggerPush   Trigger = "push"
+	TriggerManual Trigger = "tag"
+)
+
+type RootConfig struct {
+	Version  string                  `yaml:"version"`
+	Projects map[string]ProjectEntry `yaml:"projects"`
+}
+
+type ProjectEntry struct {
+	Dir string `yaml:"dir"`
+}
+
+type ProjectConfig struct {
+	Services map[string]ServiceEntry `yaml:"services"`
+}
+
+type ServiceEntry struct {
+	Name string `yaml:"name"`
+	Dir  string `yaml:"dir"`
+}
+
+type ServiceConfig struct {
+	Name       string   `yaml:"name"`
+	Provider   Provider `yaml:"provider"`
+	Trigger    Trigger  `yaml:"trigger"`
+	AutoDeploy bool     `yaml:"auto_deploy"`
+}
